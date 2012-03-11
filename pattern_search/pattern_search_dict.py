8cc013dbd26339701471dfec8f5d7f21b5ee671f
Vinicius Mendes <vbmendes@gmail.com>
2012-03-11 13:23:30 -0300

    Override pattern_search_dict __setitem__ to simulate pushing.

--- pattern_search/pattern_search_dict.py
+++ pattern_search/pattern_search_dict.py
@@ -6,6 +6,11 @@ class PatternSearchDict(dict):
     def __init__(self, get_names):
         self.get_names = get_names
 
+    def __setitem__(self, key, obj):
+        for i in xrange(1, len(key) + 1):
+            tree_node = self.setdefault(key[:i], [])
+            tree_node.append(obj)
+
     def _get_names(self, obj):
         names = self.get_names(obj)
         if hasattr(names, '__iter__'):
@@ -18,9 +23,7 @@ class PatternSearchDict(dict):
         Pushes a new object in the tree.
         """
         for name in self._get_names(obj):
-            for i in xrange(1, len(name) + 1):
-                tree_node = self.setdefault(name[:i], [])
-                tree_node.append(obj)
+            self[name] = obj
 
     def push_items(self, items):
         for obj in items:
--- tests/test_pattern_search_dict.py
+++ tests/test_pattern_search_dict.py
@@ -6,21 +6,26 @@ from pattern_search.pattern_search_dict import PatternSearchDict
 class NArySearchTree(unittest.TestCase):
 
     def setUp(self):
-        self.tree = PatternSearchDict(get_names=lambda obj: obj['name'])
+        self.dict = PatternSearchDict(get_names=lambda obj: obj['name'])
 
-    def test_tree_initialization(self):
-        self.assertIsInstance(self.tree, PatternSearchDict)
+    def test_dict_initialization(self):
+        self.assertIsInstance(self.dict, PatternSearchDict)
 
-    def test_tree_pushed_item_is_searchable(self):
+    def test_dict_pushed_item_is_searchable(self):
         item = {'name': 'natal'}
-        self.tree.push(item)
-        self.assertEqual(item, self.tree['nat'][0])
+        self.dict.push(item)
+        self.assertEqual(item, self.dict['nat'][0])
 
-    def test_tree_allow_push_more_than_one_item(self):
+    def test_dict_allow_push_more_than_one_item(self):
         items = [{'name': 'natal'}, {'name': 'rio de janeiro'}]
-        self.tree.push_items(items)
-        self.assertEqual(items[0], self.tree['nat'][0])
-        self.assertEqual(items[1], self.tree['rio'][0])
+        self.dict.push_items(items)
+        self.assertEqual(items[0], self.dict['nat'][0])
+        self.assertEqual(items[1], self.dict['rio'][0])
+
+    def test_dict_setitem_is_similar_to_pushing_the_item_with_key_as_the_name_of_the_item(self):
+        item = {'name': 'natal'}
+        self.dict['natal'] = item
+        self.assertEqual(item, self.dict['nat'][0])
 
 
 if __name__ == '__main__':
