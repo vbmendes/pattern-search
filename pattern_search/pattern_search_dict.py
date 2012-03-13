e2ae46eb4e2ad38d4e1a8e70504224a0c13b6dda
Vinicius Mendes <vbmendes@gmail.com>
2012-03-12 23:00:29 -0300

    Following @heynemann recomendation to use dict.get as search method.

--- pattern_search/pattern_search_dict.py
+++ pattern_search/pattern_search_dict.py
@@ -8,10 +8,7 @@ class PatternSearchDict(dict):
         self.get_names = get_names
 
     def __getitem__(self, key):
-        try:
-            return dict.__getitem__(self, key)
-        except KeyError:
-            return []
+        return dict.get(self, key, [])
 
     def __setitem__(self, key, obj):
         for i in xrange(1, len(key) + 1):
